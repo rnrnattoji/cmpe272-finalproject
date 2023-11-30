87a82da42e25e1b50df509c108fbb65169972717
Rajath N R <rajath.nr117@gmail.com>
2023-11-30 14:04:52 -0800

    Added index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Cross Domain Enterprise Online Market Place</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body background="online_shopping.jpeg" background-repeat="no-repeat" background-size="cover">
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- <div class="navbar-header">
+                <a class="navbar-brand">Cross Domain Enterprise Online Market Place</a>
+            </div> -->
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> New User?</a></li>
+                <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Existing User?</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="bg container" style="margin-top:60px">
+        <h1 style="text-align:center;color:white;">Welcome to Cross Domain Enterprise Online Market Place.</h1>
+    </div>
+</body>
+
+</html>
