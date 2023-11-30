766231c381a61b6c5cdfe8275490c121a4497381
subramanyajagadeesh <subramanya.grj@gmail.com>
2023-11-30 00:24:11 -0800

    Added iframe login and logout code

--- login.php
+++ login.php
@@ -1,97 +1,3 @@
-<?php
-// Initialize the session
-session_start();
-
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    // Subramanya Logic needed
-    header("location: welcome.php");
-    exit;
-}
-
-// Include config file
-require_once "config.php";
-
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = $login_err = "";
-
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // Check if username is empty
-    if (empty(trim($_POST["username"]))) {
-        $username_err = "Please enter username.";
-    } else {
-        $username = trim($_POST["username"]);
-    }
-
-    // Check if password is empty
-    if (empty(trim($_POST["password"]))) {
-        $password_err = "Please enter your password.";
-    } else {
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate credentials
-    if (empty($username_err) && empty($password_err)) {
-        // Prepare a select statement
-        $sql = "SELECT id, name, password FROM users WHERE name = ?";
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $username);
-
-            // Set parameters
-            $param_username = $username;
-
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                // Store result
-                mysqli_stmt_store_result($stmt);
-
-                // Check if username exists, if yes then verify password
-                if (mysqli_stmt_num_rows($stmt) == 1) {
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-
-                    if (mysqli_stmt_fetch($stmt)) {
-                        //echo password_hash($password, PASSWORD_DEFAULT);
-                        if (md5($password) == $hashed_password) {
-                            //if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            // session_start();
-
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
-                            // Redirect user to welcome page
-                            // Subramanya Logic needed
-                            header("location: welcome.php");
-                        } else {
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.......";
-                        }
-                    }
-                } else {
-                    // Username doesn't exist, display a generic error message
-                    $login_err = "Invalid username or password.";
-                }
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Close connection
-    mysqli_close($link);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -116,7 +22,126 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             width: 450px;
             padding: 20px;
         }
+        iframe{
+            visibility: hidden;
+        }
     </style>
+    <script>
+      function sendMessage(message) {
+        const iframe = document.querySelectorAll("iframe");
+        iframe.forEach(element => {
+          element.contentWindow.postMessage(message, "*");
+        });
+      }
+      function sendLoginMessage(user_id){
+        sendMessage({type: 'login', user_id: user_id});
+      }
+      function sendLogoutMessage(user_id){
+        sendMessage({type: "logout", user_id: user_id});
+      }
+    </script>
+    <?php
+        // Initialize the session
+        session_start();
+
+        // Check if the user is already logged in, if yes then redirect him to welcome page
+        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+            // Subramanya Logic needed
+            $user_id = $_SESSION["id"];
+            echo "
+            <script type='text/javascript'>
+                sendLoginMessage($user_id);
+            </script>";
+            header("location: welcome.php");
+            exit;
+        }
+
+        // Include config file
+        require_once "config.php";
+
+        // Define variables and initialize with empty values
+        $username = $password = "";
+        $username_err = $password_err = $login_err = "";
+
+        // Processing form data when form is submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            // Check if username is empty
+            if (empty(trim($_POST["username"]))) {
+                $username_err = "Please enter username.";
+            } else {
+                $username = trim($_POST["username"]);
+            }
+
+            // Check if password is empty
+            if (empty(trim($_POST["password"]))) {
+                $password_err = "Please enter your password.";
+            } else {
+                $password = trim($_POST["password"]);
+            }
+
+            // Validate credentials
+            if (empty($username_err) && empty($password_err)) {
+                // Prepare a select statement
+                $sql = "SELECT id, name, password FROM users WHERE name = ?";
+                if ($stmt = mysqli_prepare($link, $sql)) {
+                    // Bind variables to the prepared statement as parameters
+                    mysqli_stmt_bind_param($stmt, "s", $username);
+
+                    // Set parameters
+                    $param_username = $username;
+
+                    // Attempt to execute the prepared statement
+                    if (mysqli_stmt_execute($stmt)) {
+                        // Store result
+                        mysqli_stmt_store_result($stmt);
+
+                        // Check if username exists, if yes then verify password
+                        if (mysqli_stmt_num_rows($stmt) == 1) {
+                            // Bind result variables
+                            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+
+                            if (mysqli_stmt_fetch($stmt)) {
+                                //echo password_hash($password, PASSWORD_DEFAULT);
+                                if (md5($password) == $hashed_password) {
+                                    //if(password_verify($password, $hashed_password)){
+                                    // Password is correct, so start a new session
+                                    // session_start();
+
+                                    // Store data in session variables
+                                    $_SESSION["loggedin"] = true;
+                                    $_SESSION["id"] = $id;
+                                    $_SESSION["username"] = $username;
+
+                                    // Redirect user to welcome page
+                                    // Subramanya Logic needed
+                                    echo "
+                                    <script type='text/javascript'>
+                                        sendLoginMessage($id);
+                                    </script>";
+                                    header("location: welcome.php");
+                                } else {
+                                    // Password is not valid, display a generic error message
+                                    $login_err = "Invalid username or password.......";
+                                }
+                            }
+                        } else {
+                            // Username doesn't exist, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    } else {
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+
+                    // Close statement
+                    mysqli_stmt_close($stmt);
+                }
+            }
+
+            // Close connection
+            mysqli_close($link);
+        }
+    ?>
 </head>
 
 <body>
@@ -156,6 +181,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
+        <iframe src="https://sayali-cmpe272.bayaskarpowerpack.co.in"></iframe>
+        <iframe src="http://cmpe272.rnrnattoji.click/assignment-4"></iframe>
+        <iframe src="https://cmpe272hw.pietrasik.top"></iframe>
+        <iframe src="https://subramanyajagadeesh-0a2895b9a580.herokuapp.com"></iframe>
     </div>
 </body>
 
--- logout.php
+++ logout.php
@@ -1,13 +1,28 @@
 <?php
 // Initialize the session
 session_start();
- 
+
+//script to send the logout message to children iframe
+echo "<script>
+function sendMessage(message) {
+  const iframe = document.querySelectorAll('iframe');
+  iframe.forEach(element => {
+    element.contentWindow.postMessage(message, '*');
+  });
+}
+function sendLogoutMessage(user_id){
+  sendMessage({type: 'logout', user_id: user_id});
+}
+</script>";
+
 // Unset all of the session variables
+$id = $_SESSION['id'];
 $_SESSION = array();
- 
+
+echo "<script>sendLogoutMessage($id)</script>";
 // Destroy the session.
 session_destroy();
- 
+
 // Redirect to login page
 header("location: login.php");
 exit;
